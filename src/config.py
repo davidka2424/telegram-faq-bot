651562de297982d799547ffb211ee8b2945e5ad9
david_shamoev <136083996+davidka2424@users.noreply.github.com>
2025-10-30 14:27:30 +0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,20 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-
-# # Загружаем переменные из .env файла
-# load_dotenv()
-
-# # Токен бота из переменных окружения
-# BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-# # Проверяем что токен есть
-# if not BOT_TOKEN:
-#     raise ValueError("BOT_TOKEN не найден! Создайте файл .env с вашим токеном")
-
-# # Путь к базе данных
-# DB_PATH = "data/faq.db"
-
-
 import os
 from dotenv import load_dotenv
 
@@ -25,4 +8,5 @@ if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден!")
 
 # В Docker используем путь внутри контейнера
+
 DB_PATH = "/app/data/faq.db"
